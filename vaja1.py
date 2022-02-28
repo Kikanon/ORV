6f6710b3c623f6f44337573bfccefa5772a2817e
Gregor <jakob.srumpf@gmail.com>
2022-02-28 13:30:26 +0100

    more adjustments

--- vaja1.py
+++ vaja1.py
@@ -12,16 +12,17 @@ if not cap.isOpened():
 
 # color ranages that we will use
 boundaries = [
-    ([120, 78, 64], [186,142,125]),
-    ([151, 141, 160], [246, 227, 237]),
-    ([85,63,72], [140,129,134])
-	#([0, 0, 40], [110, 100, 200]),
+    #([120, 78, 64], [186,142,125]),
+    #([151, 141, 160], [246, 227, 237]),
+    #([85,63,72], [140,129,134]),
+	#([176,169,187], [219,227,244])
+    ([150,100,100],[255,255,255])
 ]
 
 # najdi diplomo profecorja in skopiraj
 while True:
     ret, frame = cap.read()
-    #frame = cvtColor(frame, cv2.COLOR_RGB2HSV)
+    cvtColor(frame, cv2.COLOR_RGB2HSV_FULL)
     frame = cv2.resize(frame, (320, 240), interpolation=cv2.INTER_AREA)
     #cv2.imshow('Input', frame)
 
@@ -32,12 +33,12 @@ while True:
         upper = numpy.array(upper, dtype = "uint8")
         # find the colors within the specified boundaries and apply
         # the mask
-        mak2 = frame.copy()
-        mak2[1:100,1:100,2] = 255
+        box = cv2.rectangle(frame, (10,10), (20, 20), (20,20, 20),2)
         mask = cv2.inRange(frame, lower, upper)
         output = cv2.bitwise_and(frame, frame, mask = mask)
         # show the images
-        cv2.imshow("images", numpy.hstack([frame, output, mak2]))
+        cv2.namedWindow("slikice")
+        cv2.imshow("slikice", numpy.hstack([frame, output, box]))
 
     c = cv2.waitKey(1)
     if c == ESC:

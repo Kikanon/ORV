65f6919d458e60fa8c84124b8b27a47ac272c21e
Kikanon <jakob.srumpf@gmail.com>
2022-03-12 23:17:13 +0100

    spremenil v jit kolko se da(cas racunanja je zaj nekje 3s), dodal izpis casa racunanja)... Konec v3

--- vaja3.py
+++ vaja3.py
@@ -1,15 +1,14 @@
-from ast import Constant
 from curses.ascii import ESC
 from math import sqrt
-from cv2 import INTER_MAX
 from numba import jit
+import time
 import cv2
 import numpy as np
 
 # Stevilo iteracij
 numIterations = 5
 # Stevilo skupin
-numK = 5
+numK = 8
 
 inputImage = cv2.imread('media/landscape.jpg', cv2.IMREAD_COLOR)
 
@@ -30,22 +29,31 @@ def bestFit(pixel, options):
 
     return best
 
-#@jit(nopython=True)
-def updateMedians(image, medians):
-    medianSums = np.array([np.array([0,0,0]) for _ in range(numK)])
-    medianCounts = np.array([0]*numK)
+@jit(nopython=True)
+def sumPixels(image, sums, counts, indexes):
     for x in range(image.shape[0]):
         for y in range(image.shape[1]):
-            selected = bestFit(image[x][y], medians)
-            medianSums[selected] = np.array([medianSums[selected][0]+image[x][y][0],medianSums[selected][1]+image[x][y][1],medianSums[selected][2]+image[x][y][2]])
-            medianCounts[selected] += 1
+            selected = bestFit(image[x][y], indexes)
+            sums[selected] = np.array([sums[selected][0]+image[x][y][0],sums[selected][1]+image[x][y][1],sums[selected][2]+image[x][y][2]])
+            counts[selected] += 1
+
+def updateMedians(image, medians):
+    # ustvarim arraye
+    medianSums = np.array([np.array([0,0,0]) for _ in range(numK)])
+    medianCounts = np.array([0]*numK)
     
+    # sestejem vse pixle
+    sumPixels(image, medianSums, medianCounts, medians)
+    
+    # nastavim mediane na nove vrednosti
     for i in range(len(medians)):
         medians[i] = np.divide(medianSums[i], medianCounts[i])
             
 #@jit(forceobj=True)
 def calcMedians(image):
+
     # zacetne tocke
+    #medians=[[0,0,0], [20,20,20], [255,255,255]]
     medians=np.array([[0,0,0] for _ in range(numK)])
     for i in range(1,numK+1):
         value = i * (255/(numK+1))
@@ -57,38 +65,55 @@ def calcMedians(image):
     
     return medians
 
+@jit(nopython=True)
 def reduceImage(image, colors):
     for x in range(image.shape[0]):
         for y in range(image.shape[1]):
             image[x][y] = colors[bestFit(image[x][y], colors)]
     return image
 
-#ustvarim kopijo
-outputImage = np.copy(inputImage)
+def makePalet(colors):
+    if len(colors) < 1:
+        return []
+    first_color = ( int (colors[0] [ 0 ] ), int (colors[0] [ 1 ] ), int (colors[0] [ 2 ] ))
+    pallet = np.hstack([cv2.rectangle(np.zeros((10, 10, 3), np.uint8), (0,0),(9,9), first_color, -1)])
+    for i in range(1,len(colors)):
+        square = (  int (colors[i][0]),
+                    int (colors[i][1]),
+                    int (colors[i][2]))
+        pallet = np.hstack([
+            pallet,
+            cv2.rectangle(np.zeros((10, 10, 3), np.uint8), (0,0),(9,9), square, -1)
+        ])
 
-medians = calcMedians(outputImage)
+    return pallet
 
-outputImage = reduceImage(outputImage, medians)
+if __name__ == "__main__":
+    start_time = time.time()
 
-first_color = ( int (medians[0] [ 0 ] ), int (medians[0] [ 1 ] ), int (medians[0] [ 2 ] ))
-colors = np.hstack([cv2.rectangle(np.zeros((10, 10, 3), np.uint8), (0,0),(9,9), first_color, -1)])
+    #ustvarim kopijo
+    outputImage = np.copy(inputImage)
 
-for i in range(1,len(medians)):
-    square = ( int (medians[i] [ 0 ] ), int (medians[i] [ 1 ] ), int (medians[i] [ 2 ] ))
-    colors = np.hstack([
-        colors,
-        cv2.rectangle(np.zeros((10, 10, 3), np.uint8), (0,0),(9,9), square, -1)
-    ])
+    # zracunam mediane
+    medians = calcMedians(outputImage)
 
-cv2.namedWindow("Image")
-cv2.namedWindow("Pasterized")
-cv2.namedWindow("Original")
+    # barve prilagodim medianam
+    outputImage = reduceImage(outputImage, medians)
 
-c = '0'
-while c != ESC:
-    cv2.imshow("Colors", colors)
-    cv2.imshow("Pasterized", outputImage)
-    cv2.imshow("Original", inputImage)
-    c = cv2.waitKey(500)
+    # sestavim barvne kvadratke za prikaz uporabljenih barv
+    colors = makePalet(medians)
 
-cv2.destroyAllWindows()
+    print(f"Generated in {round(time.time()-start_time, 2)}s")
+
+    cv2.namedWindow("Colors")
+    cv2.namedWindow("Pasterized")
+    cv2.namedWindow("Original")
+
+    c = '0'
+    while c != ESC:
+        cv2.imshow("Colors", colors)
+        cv2.imshow("Pasterized", outputImage)
+        cv2.imshow("Original", inputImage)
+        c = cv2.waitKey(500)
+
+    cv2.destroyAllWindows()
